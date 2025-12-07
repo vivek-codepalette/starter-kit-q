f33b9bd5f94bb42815b7aa9ae82e3b6a9bdf888b
vivek-codepalette <155106326+vivek-codepalette@users.noreply.github.com>
2025-12-07 23:01:56 +0700

    Update ConvexClientProvider.tsx

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -3,8 +3,7 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
 
-const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
-
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
+  const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
   return <ConvexProvider client={convex}>{children}</ConvexProvider>;
 }
